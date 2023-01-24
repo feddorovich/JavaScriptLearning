e34501fb44359a30810c0ae1dc722814c1130d53
feddorovich <feddorovich@outlook.com>
2023-01-24 21:46:18 +0300

    functions

--- 3_functions.js
+++ 3_functions.js
@@ -32,7 +32,39 @@ function increasePersonAge2(person) {
     return updatedPerson
 }
 const updatedPersonOne = increasePersonAge2(personeOne)
-console.log(personeOne.age)
-console.log(updatedPersonOne.age)
+//console.log(personeOne.age)
+//console.log(updatedPersonOne.age)
 
 // Колбэк функции
+
+function prinyMyName() {
+    console.log('Sergey')
+}
+console.log('Start')
+setTimeout(prinyMyName, 1000)
+// setTimeout - колбэк функция, которая вызывает функцию printMyName через 1000 милисекунд
+
+// Облати видимости JS
+// Для того чтобы JS не создавал глобальные переменные можно включить строгий режим
+// 'use strict' в начале js файла запрещает использование необъявленных переменных
+
+
+// 03:44:10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
