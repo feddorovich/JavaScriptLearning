93cfa09cc2e344d0954a356860564bb1a6e48390
feddorovich <feddorovich@outlook.com>
2023-01-26 11:49:56 +0300

    modules

--- 13_1_modules.mjs
+++ 13_1_modules.mjs
@@ -23,7 +23,14 @@ printMyName()
 // Расширения файлов моудлей не .js, а .mjs
 
 const sum = (a, b) => a + b
+
+const one = 1
+const two = 'two'
+
+
+
 export default sum
+export {one, two}
 
 
 
--- 13_2_modules.mjs
+++ 13_2_modules.mjs
@@ -1,8 +1,20 @@
 import sumNumbers from "./13_1_modules.mjs";
 // Импорт функции sumMumbers из другого файла
 // В файле из которого импортируется функция называется sum
+// При таком экспорте/импорте имена могут не совпадать
+import {
+    one as oneRenamed,
+    two
+} from './13_1_modules.mjs'
+// При таком экспорте/импорте имена переменных должны совпадать
+// Но их можно переименовывать с помощью 'as'
 
 const res1 = sumNumbers(10, 2)
 console.log(res1)
 const res2 = sumNumbers(5, 10)
 console.log(res2)
+
+console.log(oneRenamed)
+console.log(two)
+
+// 08:37:00
