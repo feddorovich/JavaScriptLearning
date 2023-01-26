8d34a6deb0f576ce2de329c9844e8aa796a2c108
feddorovich <feddorovich@outlook.com>
2023-01-26 16:55:48 +0300

    promises

--- 15_promises.js
+++ 15_promises.js
@@ -21,6 +21,29 @@ const myPromise = new Promise ((resolve, reject) => {
 https://jsonplaceholder.typicode.com/
 */
 
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(error => console.error(error))
+// response.json так же возвращает promise, потому что данных может быть много
+// второй .then покажет объект или массив объектов
+// .catch вызывается, если не пришел ответ от сервера
+
+const getData = (url) =>
+    new Promise((resolve, reject) =>
+    fetch(url)
+        .then(response => response.json())
+        .then(json => resolve(json))
+        .catch(error => reject(error))
+    )
+getData('https://jsonplaceholder.typicode.com/todos/1')
+    .then(data=> console.log(data))
+    .catch(error => reject(error))
+// Для того чтобы упростить получение промисов и получать 1 промис, а не 2
+// Создаем функцию getData, которая возвращает промис
+
+// 10:23:00
+
 
 
 
